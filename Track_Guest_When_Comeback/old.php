0a8b03e43dce57460085d1093b296ccd79120d6f
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-09 12:36:25 +0500

    i done it using set cookies

---
+++ Track_Guest_When_Comeback/old.php
@@ -0,0 +1,46 @@
+<?php
+
+include_once "config.php";
+session_start();
+
+$sessionId = session_id();
+$hashedSessionId = hash('sha256', $sessionId);
+$shortSessionId = substr($sessionId, 0, 16);
+$_SESSION['sessionId'] = $shortSessionId;
+// Regenerate the session ID to ensure freshness
+// session_regenerate_id(true);
+$_SESSION['deviceName'] = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+
+$ip_addresss = $_SERVER['REMOTE_ADDR'];
+if (isset($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip_addresss = $_SERVER['HTTP_CLIENT_IP'];
+}
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip_addresss = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip_addresss = $_SERVER['REMOTE_ADDR'];
+}
+$ip = $ip_addresss;
+$ipdat = @json_decode(file_get_contents(
+    "http://www.geoplugin.net/json.gp?ip=" . $ip
+));
+$country = $ipdat->geoplugin_countryName;
+$url = "http://ip-api.com/json/$ip";
+$data = file_get_contents($url);
+$result = json_decode($data, true);
+$isp = $result['isp'];
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+if (preg_match('/\((.*?)\)/', $user_agent, $matches)) {
+    $os_info = $matches[1];
+    $os_parts = explode(';', $os_info);
+    $version = trim($os_parts[0]);
+    $versionName = trim($os_parts[1]);
+    $model = trim($os_parts[2]);
+}
+$is_mobile = preg_match(
+    '/(android|blackberry|iphone|ipod|palm|windows\sce|symbian)/i',
+    $user_agent
+);
+$device = $is_mobile ? "Moblie" : "Dekstop";
+$sql = "INSERT INTO `users` (`ip_address`,`country`, `session_id`, `isp` ,`device`,`model`,`device_name`) VALUES ('" . mysqli_real_escape_string($conn, $ip) . "','" . mysqli_real_escape_string($conn, $country) . "','" . mysqli_real_escape_string($conn, $_SESSION['sessionId']) . "','" . mysqli_real_escape_string($conn, $isp) . "','" . mysqli_real_escape_string($conn, $device) . "','" . mysqli_real_escape_string($conn, $version . " " . " " . $versionName . " " . " " . $model) . "','" . mysqli_real_escape_string($conn, $deviceName) . "')";
+$result = mysqli_query($conn, $sql);
---
+++ Track_Guest_When_Comeback/oldwebbsite_!.php
@@ -0,0 +1,92 @@
+<?php
+include_once "helper.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+        </script>
+        <title>Testing</title>
+        <style>
+        table {
+            cursor: pointer;
+        }
+        </style>
+    </head>
+
+    <body>
+        <?php
+    getcwd();
+    ?>
+        <div class="mt-3 table-responsive">
+
+            <table class="table table-hover table-sm table-bordered">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>IP Address</th>
+                        <th>Country</th>
+                        <th>User ID</th>
+                        <th>ISP</th>
+                        <th>Device</th>
+                        <th>Device Model</th>
+                        <th>Time Stamp</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                // $count = 0;
+                $sql = "SELECT * FROM users ORDER BY Id DESC";
+                $result = $conn->query($sql);
+                if (mysqli_num_rows($result) > 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $id = $row['Id'];
+                        $ip = $row['ip_address'];
+                        $country = $row['country'];
+                        $userId = $row['session_id'];
+                        // $sessionId = preg_replace('/[^0-9]/', '', $userId);
+                        $isp = $row['isp'];
+                        $deviceName = $row['device_name'];
+                        $device = $row['device'];
+                        $model = $row['model'];
+                        $timeStamp = $row['timeStamp'];
+                        $formattedTime = date("d-m-Y H:i:s", strtotime($timeStamp));
+                        // $count++;
+                        echo "<tr>";
+                        echo "<td>$id</td>";
+                        echo "<td>$ip</td>";
+                        echo "<td>$country</td>";
+                        echo "<td>$userId</td>";
+                        echo "<td>$isp</td>";
+                        echo "<td>$device</td>";
+                        echo "<td>$model</td>";
+
+                        echo "<td>$formattedTime</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr>";
+                    echo " <td><h1>No Record Found</h1></td>";
+                    echo "</tr>";
+                }
+
+                ?>
+                </tbody>
+            </table>
+
+        </div>
+
+        <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script> -->
+        <!-- <script src="helper.js"></script> -->
+        <script></script>
+    </body>
+
+</html>
