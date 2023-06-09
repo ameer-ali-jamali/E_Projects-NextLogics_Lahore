f0417e272c4b42e44a8eb5ede22d537adb950af5
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-09 15:06:44 +0500

    javascript clicked button target class or anything

--- app.js
+++ app.js
@@ -8,3 +8,13 @@ class userInfo {
 }
 const uInfo = new userInfo();
 console.log(uInfo.age)
+
+// javascript clicked button target class or anything
+function update() {
+    var clickedButtonEvent = event.target;
+    var cardElement = clickedButtonEvent.parentNode;
+    var updatePackageInput = cardElement.querySelector('.updatePackage');
+    var packageValue = updatePackageInput.value.replace("000", "0,00")
+    updatePackageInput.value = packageValue;
+    console.log(updatePackageInput)
+}
