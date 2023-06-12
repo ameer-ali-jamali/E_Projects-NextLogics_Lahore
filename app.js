00bdea4ea5bed7f67eaf2df70592ac0f41fea9fa
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-06-12 17:00:04 +0500

    improved calcuation code

--- app.js
+++ app.js
@@ -34,3 +34,33 @@ var getPrice = price.val(calculation.toFixed(2).replace(/\.?0+$/, ''));
 var result = parseInt(getPrice.val());
 var formattedResult = result.toLocaleString();
 price.val(formattedResult)
+
+
+// Improved calculation code
+function calculatePrice() {
+    console.clear();
+    var clickedButton = $(this);
+    const numeric = new numericalDigit();
+    const findStringValue = new findString();
+    var role = clickedButton.closest('.card').find('.selected_role');
+    var smsRate = clickedButton.closest('.card').find('.updateSmsRate');
+    var package = clickedButton.closest('.card').find('.updatePackage');
+    var price = clickedButton.closest('.card').find('.updatePrice');
+    var getSmsRate = smsRate.val()
+    var getPackage = package.val().toLowerCase().replace(/\s/g, '').replace(/\,/g, '');
+    if (getPackage.match(findStringValue.lakh)) {
+        getPackage = getPackage.replace(/lakh/g, numeric.lakh).replace(/\D/g, '');
+    }
+    if (getPackage.match(findStringValue.lac)) {
+        getPackage = getPackage.replace(/lac/g, numeric.lakh).replace(/\D/g, '');
+    }
+    if (getPackage.match(findStringValue.mil)) {
+        getPackage = getPackage.replace(/million/g, numeric.million).replace(/\D/g, '');
+    }
+    let calculation = getSmsRate * getPackage;
+    var getPrice = price.val(calculation.toFixed(2).replace(/\.?0+$/, ''));
+    var updatePrice = parseInt(getPrice.val());
+    var formattedPrice = updatePrice.toLocaleString();
+    price.val(formattedPrice)
+    return formattedPrice;
+}
